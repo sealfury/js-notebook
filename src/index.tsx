2025cc589ed374d867d734255d09c73b47b4bbfb
sealfury <willspecker@gmail.com>
2021-06-16 09:10:23 +0200

    adds onDidMount props to code editor

--- src/index.tsx
+++ src/index.tsx
@@ -65,7 +65,10 @@ const App = () => {
 
   return (
     <div>
-      <CodeEditor />
+      <CodeEditor
+        initialValue='const a = 1;s'
+        onChange={value => setInput(value)}
+      />
       <textarea
         value={input}
         onChange={e => setInput(e.target.value)}
