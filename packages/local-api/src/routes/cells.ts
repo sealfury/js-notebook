51e4d5abebed12759654f74776135669d7c1444b
sealfury <willspecker@gmail.com>
2021-07-16 13:31:53 +0200

    adds functionality to route handlers

--- packages/local-api/src/routes/cells.ts
+++ packages/local-api/src/routes/cells.ts
@@ -1,22 +1,42 @@
 import express from 'express'
+import fs from 'fs/promises'
+import path from 'path'
+
+interface Cell {
+  id: string
+  content: string
+  type: 'text' | 'code'
+}
 
 export const createCellsRouter = (filename: string, dir: string) => {
   const router = express.Router()
 
+  const fullPath = path.join(dir, filename)
+
   router.get('/cells', async (req, res) => {
-    // Ensure cell storage file exists
-    // If it doesn't exist, add a default cell list
-    // Read the file
-    // Parse a list of cells from file
-    // Send list of cells back to browser
+    try {
+      // Read the file
+      const result = await fs.readFile(fullPath, { encoding: 'utf-8' })
+
+      res.send(JSON.parse(result))
+    } catch (err) {
+      // If read throws an error, inspect to see if file exists
+      if (err.code === 'ENOENT') {
+        // Add code to create a file and add default cells
+      } else {
+        throw err 
+      }
+    }
   })
 
   router.post('/cells', async (req, res) => {
-    // Ensure the file exists
-    // If not, create it
-    // Take the list of cells from req obj
-    // Serialize cell list
-    // Write cells into file
+    // Take the list of cells from req obj & serialize 
+    const { cells }: { cells: Cell[] } = req.body
+
+    // Write cells to file
+    await fs.writeFile(fullPath, JSON.stringify(cells), 'utf-8')
+
+    res.send({ status: 400 })
   })
 
   return router
